33e73a48ddc5035ce5bde04d4e4130e1f310449c
Nico Nijland <niconijland@gmail.com>
2018-03-19 14:31:35 +0100

    minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,25 @@
 // Custom jQuery
 
+// headroom.js
+$(".fixed-top").headroom({
+  "offset": 200,
+  "tolerance": 5
+});
+
+// scrollspy smooth scrolling
+$("body").scrollspy({target: ".navbar", offset: 75});
+$("#collapsibleNavbar a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
+});
+
 // toggle article content
 $(".expand").click(function( event ){
   $(event.currentTarget).parent().prev().slideToggle(400);
